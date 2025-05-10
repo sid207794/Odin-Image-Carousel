0e4de2857593c6147c44ae5439bb5f6fabef897d
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-05-10 13:26:53 +0000

    Create event listeners for next and previous buttons

--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
-class dropDown {
-  constructor(menuButton, menu, transitionTime) {
-    this.menuButton = document.querySelector(menuButton);
-    this.menu = document.querySelector(menu);
-    this.transitionTime = transitionTime;
-  }
+import './style.css';
 
-  open() {
-    this.menuButton.addEventListener('click', () => {
-      if (this.menu.classList.contains('hidden')) {
-        this.menu.classList.remove('hidden');
-        this.menu.style.display = 'block';
-        requestAnimationFrame(() => {
-          this.menu.style.opacity = '1';
-          this.menu.style.transition = `opacity ${this.transitionTime}s ease-in-out`;
-        });
-      } else {
-        this.menu.classList.add('hidden');
-        this.menu.style.opacity = '0';
-        this.menu.style.transition = `opacity ${this.transitionTime}s ease-in-out`;
-        setTimeout(() => {
-          this.menu.style.display = 'none';
-        }, this.transitionTime * 1000);
-      }
-    });
-  }
-}
+const imageNavigation = (function () {
+  const nextButton = document.querySelector('.rightButton .next');
+  const previousButton = document.querySelector('.leftButton .previous');
+  const body = document.querySelector('body');
+
+  let count = 1;
+
+  nextButton.addEventListener('click', () => {
+    const images = document.querySelectorAll('.content .images img');
+    const imagesLength = images.length;
+
+    if (count - 1 >= 0) {
+      images[count - 1].classList.remove('visible');
+      images[count - 1].classList.add('hidden');
+    } else {
+      images[imagesLength - 1].classList.remove('visible');
+      images[imagesLength - 1].classList.add('hidden');
+    }
+
+    images[count].classList.remove('hidden');
+    images[count].classList.add('visible');
+    body.style.backgroundImage = `url("${images[count].src}")`;
+
+    if (count === imagesLength - 1) {
+      count = 0;
+    } else {
+      count++;
+    }
+  });
+
+  previousButton.addEventListener('click', () => {
+    const images = document.querySelectorAll('.content .images img');
+    const imagesLength = images.length;
+
+    if (count - 1 >= 0) {
+      images[count - 1].classList.remove('visible');
+      images[count - 1].classList.add('hidden');
+    } else {
+      images[imagesLength - 1].classList.remove('visible');
+      images[imagesLength - 1].classList.add('hidden');
+    }
+
+    if (count - 2 >= 0) {
+      images[count - 2].classList.remove('hidden');
+      images[count - 2].classList.add('visible');
+      body.style.backgroundImage = `url("${images[count - 2].src}")`;
+    } else if (count === 1) {
+      images[imagesLength - 1].classList.remove('hidden');
+      images[imagesLength - 1].classList.add('visible');
+      body.style.backgroundImage = `url("${images[imagesLength - 1].src}")`;
+    } else if (count === 0) {
+      images[imagesLength - 2].classList.remove('hidden');
+      images[imagesLength - 2].classList.add('visible');
+      body.style.backgroundImage = `url("${images[imagesLength - 2].src}")`;
+    }
+
+    if (count === 0) {
+      count = imagesLength - 1;
+    } else {
+      count--;
+    }
+  });
+})();
